c479490caa1b23f6de6c99ff4c86012ccd42d197
Till Wegmüller <toasterson@gmail.com>
2017-08-04 16:52:21 +0200

    Removed Download Feature for config until after first tests

--- main.go
+++ main.go
@@ -3,19 +3,18 @@ package main
 import (
 	"flag"
 	"github.com/toasterson/opencloud/devprop"
-	"strings"
 	"github.com/toasterson/opencloud/installd"
 	"os"
 	"encoding/json"
 	"github.com/toasterson/mozaik/util"
 	"github.com/toasterson/mozaik/logger"
-	"github.com/toasterson/opencloud/common"
 )
 
 func main() {
 	confFile := flag.String("file", "NONE", "The Location of the Install Configuration file.")
 	dryRun := flag.Bool("n", false, "Only report what would be done.")
 	flag.Parse()
+	/*
 	//First Try to load the config file via http
 	configLocation , err := devprop.GetValue("install_config")
 	//Assume we have gotten a property if err is nil TODO Make sure we get error if we do not have a value
@@ -28,6 +27,7 @@ func main() {
 			panic(common.NotSupportedError("Nfs Downloads"))
 		}
 	}
+	*/
 	conf, err := os.OpenFile(*confFile, os.O_RDONLY, 0444)
 	defer conf.Close()
 	util.Must(err)
