f34afc7da3533472c7b82108f3dffc2edfbff8d9
Till Wegmüller <toasterson@gmail.com>
2017-08-08 08:33:49 +0200

    Forgot an &

--- mount/solaris.go
+++ mount/solaris.go
@@ -16,15 +16,15 @@ const (
 func MountLoopDevice(fstype string, mountpoint string, file string) error {
 	lofiadm := exec.Command(lofiadm_bin, "-a", file)
 	var lofibuff, lofierr bytes.Buffer
-	lofiadm.Stdout = lofibuff
-	lofiadm.Stderr = lofierr
+	lofiadm.Stdout = &lofibuff
+	lofiadm.Stderr = &lofierr
 	if err := lofiadm.Run(); err != nil{
 		return errors.New(strings.TrimSpace(lofierr.String()))
 	}
 	var mountbuff, mounterr bytes.Buffer
 	mount := exec.Command(mount_bin, fmt.Sprintf("-F%s", fstype), "-o", "ro", lofibuff.String(), mountpoint)
-	mount.Stdout = mountbuff
-	mount.Stderr = mounterr
+	mount.Stdout = &mountbuff
+	mount.Stderr = &mounterr
 	if err := mount.Run(); err != nil{
 		return errors.New(strings.TrimSpace(mounterr.String()))
 	}
